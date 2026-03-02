be8e6742653d431357893d4aeb276f3137786be4
gunashekar2253 <undabatlagunashekar@gmail.com>
2026-03-02 16:55:26 +0530

    Fix missing newline at end of app.js

--- app.js
+++ app.js
@@ -5,4 +5,5 @@ app.get("/", (req, res) => {
   res.send("Jenkins CI/CD Docker Demo 🚀");
 });
 
-app.listen(3000, () => console.log("App running on port 3000"));
+
+app.listen(3000, () => console.log("App running on port 3000/"));
